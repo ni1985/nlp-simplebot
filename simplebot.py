183003564f621a28691abe3222eb0ff546eb3138
Kirill Nikel <ni1985@mail.ru>
2023-11-03 20:30:18 +0000

    finished simple bots

---
+++ simplebot.py
@@ -0,0 +1,118 @@
+import tkinter.scrolledtext as tks
+
+from datetime import datetime
+from tkinter import *
+
+import os
+import openai
+openai.api_key=os.getenv('OPENAI_KEY')
+
+def get_bot_response(user_input):
+  prompt = f"Please provide a response to the following user input: '{user_input}'"
+
+  response=openai.Completion.create(model="text-davinci-002",
+    prompt=prompt,
+    max_tokens=150,
+    n=1,
+    stop=None,
+    temperature=0.5)
+  
+  bot_response=response.choices[0].text.strip()
+  return bot_response
+
+def send(event):
+    chatWindow.config(state=NORMAL)
+
+    user_input = userEntryBox.get("1.0",'end-2c')
+    user_input_lc = user_input.lower()
+    bot_response = get_bot_response(user_input_lc) 
+
+    create_and_insert_user_frame(user_input)
+    create_and_insert_bot_frame(bot_response)
+
+    chatWindow.config(state=DISABLED)
+    userEntryBox.delete("1.0","end")
+    chatWindow.see('end')
+
+def create_and_insert_user_frame(user_input):
+  userFrame = Frame(chatWindow, bg="#d0ffff")
+  Label(
+      userFrame,
+      text=user_input,
+      font=("Arial", 11),
+      bg="#d0ffff").grid(row=0, column=0, sticky="w", padx=5, pady=5)
+  Label(
+      userFrame,
+      text=datetime.now().strftime("%H:%M"),
+      font=("Arial", 7),
+      bg="#d0ffff"
+  ).grid(row=1, column=0, sticky="w")
+
+  chatWindow.insert('end', '\n ', 'tag-right')
+  chatWindow.window_create('end', window=userFrame)
+
+
+def create_and_insert_bot_frame(bot_response):
+  botFrame = Frame(chatWindow, bg="#ffffd0")
+  Label(
+      botFrame,
+      text=bot_response,
+      font=("Arial", 11),
+      bg="#ffffd0",
+      wraplength=400,
+      justify='left'
+  ).grid(row=0, column=0, sticky="w", padx=5, pady=5)
+  Label(
+      botFrame,
+      text=datetime.now().strftime("%H:%M"),
+      font=("Arial", 7),
+      bg="#ffffd0"
+  ).grid(row=1, column=0, sticky="w")
+
+  chatWindow.insert('end', '\n ', 'tag-left')
+  chatWindow.window_create('end', window=botFrame)
+  chatWindow.insert(END, "\n\n" + "")
+
+
+# main application window
+baseWindow = Tk()
+
+# set title
+baseWindow.title("The simple Bot")
+
+# set the size of the window
+baseWindow.geometry("500x250")
+
+# create a chat window as a scrolled text widget
+chatWindow = tks.ScrolledText(baseWindow, font="Arial")
+
+# Tags for message alignment: 'tag-left' for bot messages, 'tag-right' for user messages
+chatWindow.tag_configure('tag-left', justify='left')
+chatWindow.tag_configure('tag-right', justify='right')
+
+# Disable the chat window initially
+chatWindow.config(state=DISABLED)
+
+# Create the send button, with specific font, text, and background color
+# The 'command' option is commented out. Uncomment it and replace 'send' with your send function's name
+sendButton = Button(
+    baseWindow,
+    font=("Verdana", 12, 'bold'),
+    text="Send",
+    bg="#fd94b4",
+    activebackground="#ff467e",
+    fg='#ffffff',
+    command=send)
+sendButton.bind("<Button-1>", send)
+baseWindow.bind('<Return>', send)
+
+# Entry box
+userEntryBox = Text(baseWindow, bd=1, bg="white", width=38, font="Arial")
+
+# place tyhe chat window, user bow and the send button on the main window
+chatWindow.place(x=1, y=1, height=200, width=500)
+userEntryBox.place(x=3, y=202, height=27)
+sendButton.place(x=430, y=200)
+
+# Start the main event loop to keep the application running and responsive
+baseWindow.mainloop()   
